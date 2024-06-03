c843768b4ea93f5768545c2e68a9cc00b6e3047f
CatVu2002 <Catvu07082002@gmail.com>
2024-06-03 23:24:01 +0700

    Xây dựng controller xử lí đăng nhập, mua sản phẩm

--- src/index.js
+++ src/index.js
@@ -18,18 +18,27 @@ app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, 'resources/views'))
 console.log('PATH: ', path.join(__dirname, 'resources/views'))
 
-
+const CreateController = require("./app/Controllers/CreateController.js")
 const HomeController = require('./app/Controllers/HomeController')
-
 const ProductController = require("./app/Controllers/ProductController.js")
+const SearchController = require("./app/Controllers/SearchController.js")
+const OrderController = require("./app/Controllers/OrderController.js")
+const LoginController = require("./app/Controllers/LoginController.js")
 
 
 
-
+app.post('/login/check', LoginController.check);
 app.get('/trangchu', HomeController.home)
-
+app.get('/create', CreateController.create)
+app.get('/search', SearchController.search)
+app.get('/me/store', MeController.store)
 app.get('/Product/:slug', ProductController.detail)
-
+app.get('/Product/detail/:slug/buy', ProductController.buy);
+app.get('/Product/:id/edit', ProductController.edit)
+app.delete('/Product/:id', ProductController.delete)
+app.post('/create/post', CreateController.post)
+app.post('/order', OrderController.order)
+app.put('/Product/:id', ProductController.update)
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
